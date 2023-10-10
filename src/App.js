8111875d4c3dfd2be0ad6ce45628ebb040828888
Peter Sergo <pe3016se-s@student.lu.se>
2023-10-10 10:12:07 +0200

    La till header och navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavLink } from "react-router-dom";
+
+const submit = (event) => {
+  event.preventDefault();
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-4">
+      <Header />
+      <Navbar />
+      <Footer />
+
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={"monkey"} />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={"peter"} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="pb-3 mb-4 border-bottom">
+      <span className="fs-4">IMDB reviews</span>
+    </header>
+  );
+}
+
+function Navbar() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        {/* <NavLink className="nav-link" to="/compose-salad">
+          Komponera en sallad
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/view-order">
+          Visa beställningen
+        </NavLink> */}
+      </li>
+    </ul>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="pt-3 mt-4 text-muted border-top">Projekt EDAF90</footer>
   );
 }
 
